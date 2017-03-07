91699e3b190a7225d76ef3349d9459c3b06fbfd6
gleasoda [Darryl Gleason] <darryl.gleason@ymail.com>
2017-03-07 13:55:33 -0500

    Added player class

---
+++ player.py
@@ -0,0 +1,15 @@
+class Player:
+    """
+    A class object which holds player information.
+    """
+
+    def __init__(self, p_id, p_name):
+        """
+        Player initialization.
+        """
+        self.player_id = p_id
+        self.player_name = p_name
+
+
+    def __str__(self):
+        return "[%s] %s" % (self.player_id, self.player_name)
