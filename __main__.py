b374347cf53e7725540b303336bada63a502ab09
gleasoda [Darryl Gleason] <darryl.gleason@ymail.com>
2017-03-09 15:56:45 -0500

    Update user interface.

--- __main__.py
+++ __main__.py
@@ -15,22 +15,51 @@ def main(argv = None):
 
     else:
         pass
-
+    
+    print("\nEnter player names (leave blank to complete player entry);");
+    print("if a name is entered improperly, it may be reenetered later -->");
+    
     player_list = []
     while (True):
-        player_name = input("Enter player name (leave blank to complete player entry):")
-        print("\n%s" % player_name);
+        player_name = input("[Player %d]: " % (len(player_list)+1))
         if player_name == "": # entry: empty string
             break;
         new_player = Player(len(player_list)+1, player_name)
         player_list.append(new_player)
 
     if len(player_list) > 0:
-        print("Number of players: %d" % len(player_list))
+        print("\nNumber of players: %d" % len(player_list))
 
     for player in player_list:
         print(player)
+    
+    print("\nIf a name is incorrect, enter the player's ID; or, leave blank -->");
+    
+    while (True):
+        entered_id = input("\n[ID#] of player to rename: ")
+        if entered_id == "": # entry: empty string
+            break;
+        elif not entered_id.isdigit():
+            print("INVALID INPUT - player ID not found!");
+            continue;
+            
+        entered_id = int(entered_id) -1;
+        if (entered_id < 0 or entered_id >= len(player_list)):
+            print("INVALID INPUT - player ID not found!");
+            continue;
+        
+        print("Reenter the player's name; or, leave blank -->");
+        player_name = input("[Player %d]: " % (entered_id+1))
+        if player_name == "": # entry: empty string
+            continue;
+        player_list[entered_id].player_name = player_name
+        
+    if len(player_list) > 0:
+        print("\nNumber of players: %d" % len(player_list))
 
+    for player in player_list:
+        print(player)
+        
     return 0
 
 if __name__ == "__main__":
