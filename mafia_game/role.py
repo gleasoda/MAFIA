49b51974334ac15a55e85b3d238fc9357cbb0e79
gleasoda [Darryl Gleason] <darryl.gleason@ymail.com>
2017-03-10 14:56:33 -0500

    Add Role class.

---
+++ mafia_game/role.py
@@ -0,0 +1,18 @@
+class Role:
+    """
+    A class object which holds role information.
+    """
+
+    def __init__(self, r_id, r_name, r_allegiance,\
+    r_verdict):
+        """
+        Role initialization.
+        """
+        self.role_id = r_id
+        self.role_name = r_name
+        self.role_allegiance = r_allegiance
+        self.role_verdict = r_verdict
+
+    def __str__(self):
+        return "[%s] %s {%s, %s}" % (self.role_id, self.role_name,\
+        self.role_allegiance, self.role_verdict)
