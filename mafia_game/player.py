f50705efc740afd4824c8a62780c62f20a2f8791
gleasoda [Darryl Gleason] <darryl.gleason@ymail.com>
2017-03-10 15:04:44 -0500

    Adds member variables to Player.

--- mafia_game/player.py
+++ mafia_game/player.py
@@ -9,6 +9,8 @@ class Player:
         """
         self.player_id = p_id
         self.player_name = p_name
+        self.role = None
+        self.subroles = None
 
     def __str__(self):
         return "[%s] %s" % (self.player_id, self.player_name)
